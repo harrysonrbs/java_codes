be2dada948ee3739000055ceee0096b46387b673
Harryson <77166765+harrysonrbs@users.noreply.github.com>
2023-11-23 20:45:27 -0300

    first classes of my project

---
+++ src/main/java/com/projeto/spring/entity/User.java
@@ -0,0 +1,56 @@
+package com.projeto.spring.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
+	private Long id;
+	
+	@Column
+	private String name;
+	@Column
+	private Long age;
+	@Column
+	private String email;
+	
+	@ManyToMany
+	private Set<Role> roles;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getAge() {
+		return age;
+	}
+	public void setAge(Long age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}	
+}
