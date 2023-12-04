b07ab1024ef3bb39b086827d56453761f4319436
Harryson <77166765+harrysonrbs@users.noreply.github.com>
2023-12-04 14:48:21 -0300

    new methods added

--- src/main/java/com/projeto/spring/controller/DataInit.java
+++ src/main/java/com/projeto/spring/controller/DataInit.java
@@ -25,18 +25,39 @@ public class DataInit implements ApplicationListener<ContextRefreshedEvent>{
 		this.dataSource = dataSource;
 	}
 	
+	// get listAll
 	@GetMapping
 	@ResponseStatus(code = HttpStatus.OK)
 	public List<Person> findAllPeople() {
 		return dataSource.findAll();
 	}
 	
+	// get one person
 	@GetMapping("/{id}")
 	@ResponseStatus(code = HttpStatus.OK)
-	public Optional<Person> getOneUser(@PathVariable("id") Long identification) {
+	public Optional<Person> getOnePerson(@PathVariable("id") Long identification) {
 		return dataSource.findById(identification);
 	}
 	
+	// edit email one person
+	@GetMapping("/edit/{id}")
+	@ResponseStatus(code = HttpStatus.OK)
+	public void getEditOnePerson(@PathVariable("id") Long identification) {
+		Person person = dataSource.getReferenceById(identification);
+		person.setName("harryson renan");
+		person.setEmail("harrysousa247@gmail.com");
+		dataSource.save(person);
+	}
+	
+	
+	// delete one person
+	@GetMapping("/delete/{id}")
+	@ResponseStatus(code = HttpStatus.OK)
+	public void deletePerson(@PathVariable("id") Long identification) {
+		dataSource.deleteById(identification);
+	}
+	
+	// save new person
 	public void newPerson(String name, String email, String city, String country) {
 		Person newUsers = new Person(name, email, city, country);
 		dataSource.save(newUsers);
